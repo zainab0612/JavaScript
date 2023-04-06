89a50457c749e5b6b36cc9bd318c19b9253c784c
Zainab0612 <znaeem0612@gmail.com>
2023-04-06 16:56:16 +0500

    FEAT: JavaScript Task 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
+// JavaScript Task 1
+
 var ZakatPercentage = 0.025
-var UserInput = +prompt('Enter Your Zakat Value:')
+var UserInput = +prompt('Enter Your Total Amount:')
 var Result = ZakatPercentage * UserInput
 alert("Your Zakat Value is" +" "+Result)
